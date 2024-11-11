6ac9910af639a282ae5d7e1212565a64bbdf00d6
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-11 23:14:42 +0530

    Switched from cdn image to ftp image download to increase speed

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ SCRIPT_PATH="$(readlink -f "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 XML_FILE="/tmp/openbsd-vm-base.xml"
 
-LATEST_IMAGE=$(lynx -dump -listonly -nonumbers https://cdn.openbsd.org/pub/OpenBSD/ | grep [0-9] | head -n 1 | xargs -I {} lynx -dump -listonly -nonumbers {}amd64/ | grep install73.iso)
+LATEST_IMAGE=$(lynx -dump -listonly -nonumbers https://ftp.openbsd.org/pub/OpenBSD/ | grep [0-9] | head -n 1 | xargs -I {} lynx -dump -listonly -nonumbers {}amd64/ | grep install73.iso)
 
 echo y | ship --vm delete openbsd-vm-base 
 
@@ -12,8 +12,11 @@ echo n | ship --vm create openbsd-vm-base --source "$LATEST_IMAGE"
 
 sed -i '/<\/devices>/i \
   <console type="pty">\
-    <target type="virtio"/>\
-  </console>' "$XML_FILE"
+    <target type="serial" port="0"/>\
+  </console>\
+  <serial type="pty">\
+    <target port="0"/>\
+  </serial>' "$XML_FILE"
 
 virsh -c qemu:///system undefine openbsd-vm-base
 virsh -c qemu:///system define "$XML_FILE"
